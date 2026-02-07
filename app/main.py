709ea57ac0d4dd99c226ad4ad3d8399a830756a1
Ruz <ruz@example.com>
2026-02-07 13:08:50 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,7 +17,31 @@ class EchoCommand(Command):
     def run( self, args ):
         print( " ".join(args) )
         
-           
+class TypeCommand(Command):
+    
+    def run(self, args):
+        for arg in args:
+            if arg in ["exit", "echo", "type"]:
+                print(arg + " is a shell builtin")
+                
+            else: 
+                
+                output = arg + ": not found"
+                
+                path = os.environ.get("PATH")
+                
+                for p in path.split(":"):
+                    
+                    if os.path.isdir(p):
+                        
+                        for f in os.listdir(p):
+                            
+                            f2 = os.path.join(p, f)
+                            
+                            if f == arg and os.path.isfile( f2 ) and os.access(f2, os.X_OK) : 
+                                output = arg + " is " + f2
+                
+                print(output)
     
 def main():
     
@@ -34,28 +58,7 @@ def main():
             EchoCommand().run(args)
             
         elif command == "type":
-            for arg in args:
-                if arg in ["exit", "echo", "type"]:
-                    print(arg + " is a shell builtin")
-                    
-                else: 
-                    
-                    output = arg + ": not found"
-                    
-                    path = os.environ.get("PATH")
-                    
-                    for p in path.split(":"):
-                        
-                        if os.path.isdir(p):
-                            
-                            for f in os.listdir(p):
-                                
-                                f2 = os.path.join(p, f)
-                                
-                                if f == arg and os.path.isfile( f2 ) and os.access(f2, os.X_OK) : 
-                                    output = arg + " is " + f2
-                    
-                    print(output)
+            TypeCommand().run(args)            
   
         else:
             print(f"{command}: command not found")
