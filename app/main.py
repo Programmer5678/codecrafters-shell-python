98da990b8893cec559a879abb9e0e5a99868dac6
Ruz <ruz@example.com>
2026-02-07 11:08:57 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,11 +10,12 @@ def main():
         sys.stdout.flush()
         line = sys.stdin.readline()
         firstWord = line.split()[0]
-        if invalid(firstWord):
-            sys.stdout.write(f"{firstWord}: command not found\n")
-            
+        
         if firstWord == "exit":
             exit(-1)
+            
+        if invalid(firstWord):
+            sys.stdout.write(f"{firstWord}: command not found\n")
     
 
 
