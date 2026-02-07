da70cdf7252d1dd19c2b2308ca1822d5671ba96d
Ruz <ruz@example.com>
2026-02-07 11:03:49 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 import sys
 
+def invalid(word):
+    return True
 
 def main():
     sys.stdout.write("$ ")
+    sys.stdout.flush()
+    line = sys.stdin.readline()
+    firstWord = line.split()[0]
+    if invalid(firstWord):
+        sys.stdout.write(f"{firstWord}: command not found")
     pass
 
 
