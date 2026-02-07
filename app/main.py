0d36c0e085938b0e19c0998d59ffaad369f6bbb3
Ruz <ruz@example.com>
2026-02-07 11:06:26 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ def invalid(word):
     return True
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-    line = sys.stdin.readline()
-    firstWord = line.split()[0]
-    if invalid(firstWord):
-        sys.stdout.write(f"{firstWord}: command not found")
-    pass
+    
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        line = sys.stdin.readline()
+        firstWord = line.split()[0]
+        if invalid(firstWord):
+            sys.stdout.write(f"{firstWord}: command not found\n")
+    
 
 
 if __name__ == "__main__":
