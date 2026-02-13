fa666ebe3f93fdccad405be2bc8baa746d942b96
Ruz <ruz@example.com>
2026-02-13 13:42:04 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import subprocess
 
 import readline
 import sys
-from typing import List
+from typing import Any, List
 
 
 
@@ -151,21 +151,18 @@ class TypeCommand(BuiltinCommandInvoc):
     
     def run(self, stdin):
 
-        def _shell_builtin(arg):
-            return arg in BuiltinCommandInvoc._commands
+        def _print_shell_builtin(com):
+            print(com + " is a shell builtin")
 
-        def _print_shell_builtin(arg):
-            print(arg + " is a shell builtin")
+        def _print_exec(com, executable):
+            print(com + " is " + executable.full_path())
 
-        def _print_exec(arg, executable):
-            print(arg + " is " + executable.full_path())
-
-        def _err_not_found(arg):
-            print(arg + ": not found")
+        def _err_not_found(com):
+            print(com + ": not found")
 
         # ---- main loop ----
         for arg in self.spec().args():
-            if _shell_builtin(arg):
+            if BuiltinCommandInvoc.is_builtin(arg):
                 _print_shell_builtin(arg)
             else:
                 executable = File.find_in_path(arg)
