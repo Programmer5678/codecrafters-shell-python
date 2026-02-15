2e419784417dc822e9bd32ecce529454619f103c
Ruz <ruz@example.com>
2026-02-15 18:02:56 +0200

    codecrafters submit [skip ci]

--- app/command_invoc/subtypes/buitlin/subtypes/echo.py
+++ app/command_invoc/subtypes/buitlin/subtypes/echo.py
@@ -9,26 +9,29 @@ class EchoCommand(BuiltinCommandInvoc):
 
     def run( self, stdin ):
         
+                
         if self.end_pipe():
             next_stdin = None
-            p = Process(target=self.on_way, args=( 1 ,))
-            p.start()
+            
+            child_pid = os.fork()
+            if child_pid == 0:
+                self.on_way(1)
+                os._exit(0)
             
         else:
             next_stdin, stdout = os.pipe()
-            # os.write(stdout, b"hi")
+                  
+            child_pid = os.fork()   
+            if child_pid == 0:
+                self.on_way(stdout)
+                os._exit(0)
             
-            print( "A Proc: ", os.getpid() , " parent : ", os.getppid() )
-            print( "Fd stat :", os.fstat(stdout) )
-            
-            p = Process(target=self.on_way, args=(stdout,))
-            p.start()
             os.close(stdout)
         
         if stdin:
             os.close(stdin)
         
-        return next_stdin, lambda : p.join()
+        return next_stdin, lambda : os.waitpid( child_pid , 0)
     
     def on_way(self, out):
         
@@ -39,7 +42,5 @@ class EchoCommand(BuiltinCommandInvoc):
     
     def actual_run(self, out):
         
-        print( "Proc: ", os.getpid() , " parent : ", os.getppid() )
-        print( "Fd stat :", os.fstat(out) )
         os.write( out, (" ".join( self.spec().args()) + "\n").encode() )
         
