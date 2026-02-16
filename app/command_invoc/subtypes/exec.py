c0685b4226cff0930d92d86b01af734f96b73ab2
Ruz <ruz@example.com>
2026-02-16 18:57:01 +0200

    codecrafters submit [skip ci]

--- app/command_invoc/subtypes/exec.py
+++ app/command_invoc/subtypes/exec.py
@@ -11,36 +11,45 @@ class ExecCommandInvoc(CommandInvoc):
 
     def run(self, stdin):
 
+
+        #SAME
         def proc_filedescriptors():
             """Return (next_stdin, stdout) for this process stage."""
             return (None, STDOUT) if self.end_pipe() else os.pipe()
-
-        def run_in_child(out_fd):
-            """Fork and run the child logic, exiting immediately."""
-            child_pid = os.fork()
-            if child_pid == 0:
-                
-                os.dup2(stdin, STDIN)
-                os.dup2(out_fd, STDOUT)
-                os.execvp( self.spec().command() , [ self.spec().command(), *self.spec().args() ] )
-                
-            return child_pid
-
+        
+        #SAME
         def parent_close_fds(out_fd, in_fd):
             """Close file descriptors the parent does not need."""
             if out_fd != STDOUT:
                 os.close(out_fd)
             if in_fd != None and in_fd != STDIN :
                 os.close(in_fd)
-
-
-        next_stdin, stdout = proc_filedescriptors()
-        child_pid = run_in_child(stdout)
-        parent_close_fds(stdout, stdin)
-        result = PipelineResult(next_stdin, lambda: os.waitpid(child_pid, 0) )
-
-        return result        
         
+        #SAME        
+        def run_in_new_proc():
+            next_stdin, stdout = proc_filedescriptors()
+            child_pid = run_in_child(stdin, stdout)
+            parent_close_fds(stdout, stdin)
+            return PipelineResult(next_stdin, lambda: os.waitpid(child_pid, 0) )
+        
+        # DIFF
+        def run_in_child(in_fd, out_fd):
+            """Fork and run the child logic, exiting immediately."""
+            child_pid = os.fork()
+            if child_pid == 0:
+                
+                os.dup2(in_fd, STDIN)
+                os.dup2(out_fd, STDOUT)
+                os.execvp( self.spec().command() , [ self.spec().command(), *self.spec().args() ] )
+                
+            return child_pid
+        
+        
+
+        return run_in_new_proc()  
+        
+        
+
 
     # def run(self, stdin):
     #     # Start the process
