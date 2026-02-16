7c3c4bbf411e8399f342d736d52dfb6424fc6477
Ruz <ruz@example.com>
2026-02-16 19:38:31 +0200

    codecrafters submit [skip ci]

--- app/command_invoc/subtypes/notfound.py
+++ app/command_invoc/subtypes/notfound.py
@@ -5,6 +5,10 @@ def err_not_found(command):
     print(f"{command}: command not found", file=sys.stderr) 
 
 class NotFoundCommandInvoc (CommandInvoc):
+    
+    def _run_in_child(self, in_fd, out_fd):
+        raise Exception("FUCK")
+    
     def run(self, stdin):
         return err_not_found(
             self.spec().command()
