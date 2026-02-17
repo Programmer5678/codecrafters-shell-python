74298a5b21fd2c25948f52a20b39f3c356238d3c
Ruz <ruz@example.com>
2026-02-17 19:46:40 +0200

    codecrafters submit [skip ci]

--- app/command_invoc/models.py
+++ app/command_invoc/models.py
@@ -54,10 +54,6 @@ class CommandInvocSpec:
             outside_double_quotes = True
 
             for index, c in enumerate(st):
-                # Debug print
-                # print(f"Index: {index}, Char: {repr(c)}, "
-                #     f"outside_single_quotes: {outside_single_quotes}, "
-                #     f"outside_double_quotes: {outside_double_quotes}")
 
                 if outer_space(c):
                     result += tokenize(st[index + 1:])
@@ -68,10 +64,10 @@ class CommandInvocSpec:
                 if c == SINGLE_QUOTE and not outside_single_quotes:
                     outside_single_quotes = True
 
-                if c == DOUBLE_QUOTE and not outside_double_quotes:
+                elif c == DOUBLE_QUOTE and not outside_double_quotes:
                     outside_double_quotes = True
 
-                if outside_double_quotes and outside_single_quotes:
+                elif outside_double_quotes and outside_single_quotes:
                     if c == SINGLE_QUOTE:
                         outside_single_quotes = False
                     if c == DOUBLE_QUOTE:
