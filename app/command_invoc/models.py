ef422fc47b1c6e2cba1c77161a9dcb1ce7c3cba0
Ruz <ruz@example.com>
2026-02-18 08:39:10 +0200

    codecrafters submit [skip ci]

--- app/command_invoc/models.py
+++ app/command_invoc/models.py
@@ -115,12 +115,12 @@ def _tokenize(st):
             elif is_opening_double_quote(c, prev_backslash):
                 open_double_quote()
 
-            else:
-                add_char(result, c)
-                if c == BACKSLASH:
-                    prev_backslash = True
+            elif c == BACKSLASH:
+                prev_backslash = True
                 continue
-            
+            else:  
+                add_char(result, c)
+                
             
             if prev_backslash:
                 prev_backslash = False
